52944e770f05de60850a00acb1e12149418eeaa5
Marya Faivisovich <z89262554967@gmail.com>
2022-12-22 15:44:44 +0300

    Task_10

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -4,3 +4,19 @@
 // 782 -> 8
 // 918 -> 1
 
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите трехзначное число");
+        var n = Math.Abs(Convert.ToInt64(Console.ReadLine()));
+
+        while (n >= 100)
+        {
+            n /= 10;
+        }
+
+        var r = n % 10;
+
+        Console.WriteLine("Вторая цифра: + r");
+    }
+}
