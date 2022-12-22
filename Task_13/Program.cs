8f0a46c5f949557224a55922c0bca4321d9d4e42
Marya Faivisovich <z89262554967@gmail.com>
2022-12-22 15:39:19 +0300

    Task_13

--- Task_13/Program.cs
+++ Task_13/Program.cs
@@ -3,3 +3,24 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+
+
+            Console.Write("Введите любое число: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int lastThreeDigits = num % 100;
+
+            if (lastThreeDigits <= 0)
+            {
+                Console.WriteLine("третьей цифры нет");
+            }
+            else
+            {
+                if (lastThreeDigits > 3)
+                {
+                    num = num % 10;
+                    num = num / Convert.ToInt32(Math.Pow(10, lastThreeDigits - 3)); 
+                }
+                
+                Console.WriteLine($"Третья цифра введённого числа: {num}");
+            }
+            
