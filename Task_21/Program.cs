f0df2249d139eb1c0f187211c2d3675079577f42
Marya Faivisovich <z89262554967@gmail.com>
2022-12-25 18:30:44 +0300

    YES

--- Task_21/Program.cs
+++ Task_21/Program.cs
@@ -6,3 +6,17 @@
 
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
+double GetDistanceBetweenthreePoints(int xA, int yA, int zA, int xB, int yB, int zB)
+{
+    double distanceBetweenthreePoints = Math.Sqrt((xB-xA)*(xB-xA) + (yB-yA)*(yB-yA) + (zB-zA)*(zB-zA));
+    return distanceBetweenthreePoints;
+}
+int xA = 3;
+int yA = 6;
+int zA = 8;
+int xB = 2;
+int yB = 1;
+int zB = -7;
+
+double distance = GetDistanceBetweenthreePoints(xA, yA, zA, xB, yB, zB);
+Console.WriteLine($"Расстояние между точками А и В равно {distance:f}");
