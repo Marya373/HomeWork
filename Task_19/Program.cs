6ac0c9aa2d3dee244901a1393425e2ed15784c77
Marya Faivisovich <z89262554967@gmail.com>
2022-12-25 18:11:30 +0300

    Task19

--- Task_19/Program.cs
+++ Task_19/Program.cs
@@ -6,3 +6,30 @@
 // 12821 -> да
 
 // 23432 -> да
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите пятизначное число:");
+        string number = Console.ReadLine();
+        int len = number.Length;
+
+        if (len == 5);
+
+        {
+            if (number[0] == number[4] && number[1] == number[3])
+            {
+                Console.WriteLine("${number} - палиндром");
+            }
+            else
+            {
+                Console.WriteLine("${number} - не палиндром");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Ошибка: {number} - не является пятизначным");
+        }
+    }
+    
