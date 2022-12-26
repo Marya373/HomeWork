41e6413bf2ac8b9534f3c43ebfada036e8e171a6
Marya Faivisovich <z89262554967@gmail.com>
2022-12-26 15:36:39 +0300

    19_21_Rabotaet

--- Task_19/Program.cs
+++ Task_19/Program.cs
@@ -7,21 +7,21 @@
 
 // 23432 -> да
 
-
 Console.WriteLine("Введите пятизначное число:");
-        string number = Console.ReadLine()!;
-        int len = number.Length;
+string input = Console.ReadLine()!;
+
+if (input.Length == 5)
 {
-        if (len == 5)
-        {        
-            if (number[0] == number[4] && number[1] == number[3])
-            {
-                Console.WriteLine("Введенное число - палиндром");
-            }
-                   
-        else
-        {
-            Console.WriteLine("Введенное число не пятизначное");
-        }
-        }
+    if (input[0] == input[4] && input[1] == input[3])
+    {
+        Console.WriteLine("Введенное число - палиндром");
+    }
+    else
+    {
+        Console.WriteLine("Введенное число - не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine("Введенное число не пятизначное");
 }
--- Task_21/Program.cs
+++ Task_21/Program.cs
@@ -6,10 +6,10 @@
 
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-double GetDistanceBetweenthreePoints(int xA, int yA, int zA, int xB, int yB, int zB)
+double GetDistance3D(int xA, int yA, int zA, int xB, int yB, int zB)
 {
-    double distanceBetweenthreePoints = Math.Sqrt((xB-xA)*(xB-xA) + (yB-yA)*(yB-yA) + (zB-zA)*(zB-zA));
-    return distanceBetweenthreePoints;
+    double distance3D = Math.Sqrt((xB-xA)*(xB-xA) + (yB-yA)*(yB-yA) + (zB-zA)*(zB-zA));
+    return distance3D;
 }
 int xA = 3;
 int yA = 6;
@@ -18,5 +18,5 @@ int xB = 2;
 int yB = 1;
 int zB = -7;
 
-double distance = GetDistanceBetweenthreePoints(xA, yA, zA, xB, yB, zB);
+double distance = GetDistance3D(xA, yA, zA, xB, yB, zB);
 Console.WriteLine($"Расстояние между точками А и В равно {distance:f}");
