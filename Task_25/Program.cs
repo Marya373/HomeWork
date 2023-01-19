727cd3e335ab5d8d2f4aa8344478cbee13104fac
Faivisovich Marya <z89262554967@gmail.com>
2023-01-19 18:17:23 +0300

    25_27

--- Task_25/Program.cs
+++ Task_25/Program.cs
@@ -3,33 +3,34 @@
 //3, 5 -> 243 (3⁵)
 //2, 4 -> 16
 
-//Console.Write("Введите число: ");
-//int A = int.Parse(Console.ReadLine()!);
+int Degree(int numberA, int numberB)
+{
+  int result = 1;
+  for(int i=1; i <= numberB; i++){
+    result = result * numberA;
+  }
+    // int result = Math.Pow(numberA, numberB);
+    return result;
+}
 
-//Console.Write("Введите степень числа: ");
-//int B = int.Parse(Console.ReadLine()!);
+  Console.Write("Введите число A: ");
+  int numberA = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите число B: ");
+  int numberB = Convert.ToInt32(Console.ReadLine());
 
-//int res = 1;
+  int degree = Degree(numberA, numberB);
+  Console.WriteLine("Ответ: " + degree);
 
-//for (int i = 1; i <= B; i++)
-//{
-//    res = A^B;
-//}
-//return res;
+//double A;
+//double B;
+//double C;
 
-//Console.WriteLine({res});  
-// Клим, не могу найти ошибку цикла. Решила другим способом
+//Console.WriteLine("Введите число, которое хотите возвести в степень: ");
+//A = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("Введите степень числа "+A+"");
+//B = Convert.ToDouble(Console.ReadLine());
 
-double A;
-double B;
-double C;
-
-Console.WriteLine("Введите число, которое хотите возвести в степень: ");
-A = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Введите степень числа "+A+"");
-B = Convert.ToDouble(Console.ReadLine());
-
-C = Math.Pow(A, B);
-Console.WriteLine("результат "+A+" в степени "+B+" равно "+C+"!");
-Console.ReadLine();
+//C = Math.Pow(A, B);
+//Console.WriteLine("результат "+A+" в степени "+B+" равно "+C+"!");
+//Console.ReadLine();
 
