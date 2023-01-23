b2dca9ce2bdbf6168b7a4ba2c15c36e447f82424
Faivisovich Marya <z89262554967@gmail.com>
2023-01-23 09:25:42 +0300

    47

--- Task_47/Program.cs
+++ Task_47/Program.cs
@@ -4,3 +4,36 @@
 //0,5 7 -2 -0,2
 //1 -3,3 8 -9,9
 //8 7,8 -7,1 9
+
+Console.WriteLine("введите количество строк");
+int linesNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int columnsNum = Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[linesNum, columnsNum];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-50, 50)) / 10;
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
