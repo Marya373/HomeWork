411d56b59b1ed6a4248d2e8ae839afe489d03b68
Faivisovich Marya <z89262554967@gmail.com>
2023-01-29 11:46:31 +0300

    12

--- Task_68/Program.cs
+++ Task_68/Program.cs
@@ -16,10 +16,12 @@ int InputInt(string output)
 
 int Akkerman(int m, int n)
 {
-    if (m == 0)
-        return n + 1;
-    if (m > 0 && n == 0)
-        return Akkerman(m - 1, 1);
-    else
-        return Akkerman(m - 1, Akkerman(m, n - 1));
+    {
+        if (m == 0)
+            return n + 1;
+        if (m > 0 && n == 0)
+            return Akkerman(m - 1, 1);
+        else
+            return Akkerman(m - 1, Akkerman(m, n - 1));
+    }
 }
